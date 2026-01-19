b1c0bb62794f8cd30bb2104c35d50b8dc67a535a
anoopa413 <anoopa413@gmail.com>
2026-01-19 20:29:05 +0530

    Remove obsolete migration files and associated SQL commands for various schema changes including dropping columns, tables, and foreign key constraints across multiple entities such as Campaign, Template, Contact, and others. This cleanup addresses previously added fields and constraints that are no longer needed in the database schema update

--- prisma/migrations/20260102145721_add_is_name_field_to_custom_field/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "CustomField" ADD COLUMN     "isNameField" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20260113183942_mailgun/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `subject` on the `Template` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Template" DROP COLUMN "subject";
