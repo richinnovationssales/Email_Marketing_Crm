ec898d5b07551a200e733ebba699841fca2c9c64
Trideep.H <trideep.h@ivldsp.com>
2026-01-19 16:48:36 +0530

    change-in-cors-policy-issue

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,10 @@ const stream = {
 
 app.use(morgan('tiny', { stream }));
 app.use(cors({
-  origin: '*',
+  origin: [
+    'https://email-marketing-web.vercel.app'
+  ],
+  credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
