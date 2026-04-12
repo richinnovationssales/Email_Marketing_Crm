37e08422f289e1c6c208f2957781cf2c28b17691
Trideep <trideepkumar.h@gmail.com>
2026-04-12 10:17:49 +0530

    contact route changes

--- src/presentation/routes/contactRoutes.ts
+++ src/presentation/routes/contactRoutes.ts
@@ -12,11 +12,13 @@ const contactController = new ContactController();
 router.use(authMiddleware);
 router.use(checkClientApproval);
 
-router.post('/', contactController.createContact);
 router.get('/search', contactController.searchContacts); 
+router.post('/', contactController.createContact);
+router.get('/:id', contactController.getContactById);
+
 router.post('/upload', upload.single('file'), contactController.bulkUpload);
 router.get('/', contactController.getContacts);
-router.get('/:id', contactController.getContactById);
+
 router.put('/:id', contactController.updateContact);
 router.post('/bulk-delete', contactController.bulkDeleteContacts);
 router.delete('/:id', contactController.deleteContact);
